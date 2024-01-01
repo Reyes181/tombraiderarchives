6eeda5922d8c0d72a00936f03c860c1eb42be530
emilio reyes <emilioreyes@emilios-MacBook-Air.local>
2024-01-01 18:36:25 -0500

    added fallback for null in archive page

--- app/archives/[id]/page.tsx
+++ app/archives/[id]/page.tsx
@@ -54,8 +54,8 @@ export async function generateMetadata({
 
 const ArchivePage = async ({ params }: {params: IParams}) => {
     const archive = await getArchiveById(params);
-    const comics = await getComicsById(params);
-    const arts = await getArtsById(params);
+    // const comics = await getComicsById(params);
+    // const arts = await getArtsById(params);
     const gallery = await getGalleryById(params);
 
     return (
@@ -113,7 +113,7 @@ const ArchivePage = async ({ params }: {params: IParams}) => {
                             <span>Available To Read</span>
                             <hr className="w-1/4 h-0.5 bg-teal-500"/>
                         </div>
-                        
+                        {/* <Suspense fallback={<CardsSkeleton/>}>
                             <div className="px-4 grid grid-cols-1 md:grid-cols-4 xl:grid-cols-5 gap-4 lg:gap-10 xl:gap-6">
                             {comics?.map((comic, i) => (
                                     <ComicCard
@@ -124,7 +124,7 @@ const ArchivePage = async ({ params }: {params: IParams}) => {
                                     </ComicCard>
                             ))}
                             </div>
-                        
+                        </Suspense> */}
                     </div>
                 </Container>
             </section>
@@ -146,7 +146,7 @@ const ArchivePage = async ({ params }: {params: IParams}) => {
                             <hr className="w-1/4 h-0.5 bg-teal-500"/>
                         </div>
                     </div>
-                    <Suspense fallback={<CardsSkeleton/>}>
+                    {/* <Suspense fallback={<CardsSkeleton/>}>
                         <SliderContainer>
                             <>
                             {arts?.map((art, i) => (
@@ -156,7 +156,7 @@ const ArchivePage = async ({ params }: {params: IParams}) => {
                             ))}
                             </> 
                         </SliderContainer>
-                    </Suspense>
+                    </Suspense> */}
                 </Container>
             </section>
             <section className="w-full pt-16 pb-28">
