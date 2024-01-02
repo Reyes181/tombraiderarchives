cfda62580fede8cd71fe59481dec3122d930f3c9
emilio reyes <emilioreyes@emilios-MacBook-Air.local>
2024-01-02 14:08:50 -0500

    created a grey placeholder for comics

--- app/archives/[id]/ArchivesComics.tsx
+++ app/archives/[id]/ArchivesComics.tsx
@@ -6,7 +6,6 @@ import { getComicsById} from "../../actions/getComics";
 import Image from "next/image";
 import fs from "node:fs/promises";
 import { getPlaiceholder } from "plaiceholder";
-import DynamicBlur from "../../utils/DynamicBlur";
 
 interface IParams {
     id?: string
@@ -14,19 +13,11 @@ interface IParams {
 
 const ArchivesComics = async ({ params }: {params: IParams}) => {
     const comics = await getComicsById(params);
-    // const src = '/images/greyplaceholder.jpg';
+    const src = '/images/greyplaceholder.jpg';
 
-    // const buffer = await fs.readFile(`./public${src}`);
+    const buffer = await fs.readFile(`./public${src}`);
 
-    // const { base64 } = await getPlaiceholder(buffer);
-
-    // const src = ""
-
-    // const buffer = await fetch(src).then( async (res) => {
-    //     return Buffer.from(await res.arrayBuffer());
-    // })
-
-    // const { base64 } = await getPlaiceholder(buffer);
+    const { base64 } = await getPlaiceholder(buffer);
     return(
 <       Suspense fallback={<CardsSkeleton/>}>
             <div className="px-4 grid grid-cols-1 md:grid-cols-4 xl:grid-cols-5 gap-4 lg:gap-10 xl:gap-6">
@@ -36,7 +27,7 @@ const ArchivesComics = async ({ params }: {params: IParams}) => {
                         key={i}
                     >
                         {/* <BlurImage name={comic.name} mainImage={comic.cover} comic/> */}
-                        {/* <Image
+                        <Image
                             placeholder='blur'
                             blurDataURL={base64}
                             src={comic.cover}
@@ -46,8 +37,8 @@ const ArchivesComics = async ({ params }: {params: IParams}) => {
                             className={
                                 'cursor-pointer shrink-0 drop-shadow-lg w-auto ease-in-out h-auto md:h-3/4 lg:h-5/6 duration-300 hover:-translate-y-2'
                             }
-                        /> */}
-                        <DynamicBlur mainImage={comic.cover} name={comic.name}/>
+                        />
+                        {/* <DynamicBlur mainImage={comic.cover} name={comic.name}/> */}
                     </ComicCard>
                     
             ))}
