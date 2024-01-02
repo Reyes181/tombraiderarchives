6253328863196c6eb44fd36cb75b8a826bd2684f
emilio reyes <emilioreyes@emilios-MacBook-Air.local>
2024-01-02 15:09:01 -0500

    created a grey placeholder for comics

--- app/utils/BlurImage.tsx
+++ app/utils/BlurImage.tsx
@@ -32,22 +32,21 @@ const getImage = async (src: string) => {
     const {base64, img} = await getImage(props.mainImage);
 
     return (
-        // <Image
-        //     {...img}
-        //     alt={props.name}
-        //     title={props.name}
-        //     blurDataURL={base64}
-        //     placeholder='blur'
-        //     className={
-        //         props.comic ? 'cursor-pointer shrink-0 drop-shadow-lg w-auto ease-in-out h-auto md:h-3/4 lg:h-5/6 duration-300 hover:-translate-y-2'
-        //         :
-        //         props.dhcomic ? `shrink-0 drop-shadow-lg w-auto ease-in-out h-auto md:h-3/4 ${props.random ? 'lg:h-3/4' : 'lg:h-5/6'} duration-300 hover:-translate-y-2`
-        //         :
-        //         props.arch ? "object-contain object-center h-auto w-auto"
-        //         :
-        //         ''
-        //     }
-        // />
-        <div>{props.mainImage}</div>
+        <Image
+            {...img}
+            alt={props.name}
+            title={props.name}
+            blurDataURL={base64}
+            placeholder='blur'
+            className={
+                props.comic ? 'cursor-pointer shrink-0 drop-shadow-lg w-auto ease-in-out h-auto md:h-3/4 lg:h-5/6 duration-300 hover:-translate-y-2'
+                :
+                props.dhcomic ? `shrink-0 drop-shadow-lg w-auto ease-in-out h-auto md:h-3/4 ${props.random ? 'lg:h-3/4' : 'lg:h-5/6'} duration-300 hover:-translate-y-2`
+                :
+                props.arch ? "object-contain object-center h-auto w-auto"
+                :
+                ''
+            }
+        />
     )
   }
