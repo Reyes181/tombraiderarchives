03fcce60bc212b653285d808813566a7afa90ace
emilio reyes <emilioreyes@emilios-MacBook-Air.local>
2024-01-01 20:41:45 -0500

    add ? to blurimage props

--- app/archives/[id]/ArchivesComics.tsx
+++ app/archives/[id]/ArchivesComics.tsx
@@ -3,6 +3,7 @@ import CardsSkeleton from "../../utils/CardsSkeleton";
 import ComicCard from "../../components/cards/ComicCard";
 import BlurImage from "../../utils/BlurImage";
 import { getComicsById} from "../../actions/getComics";
+import Image from "next/image";
 
 interface IParams {
     id?: string
@@ -18,7 +19,16 @@ const ArchivesComics = async ({ params }: {params: IParams}) => {
                         comic={comic}
                         key={i}
                     >
-                        <BlurImage name={comic.name} mainImage={comic.cover} dhcomic/>
+                        {/* <BlurImage name={comic.name} mainImage={comic.cover} comic/> */}
+                        <Image
+                            src={comic.cover}
+                            alt={comic.name}
+                            width={500}
+                            height={500}
+                            className={
+                                'cursor-pointer shrink-0 drop-shadow-lg w-auto ease-in-out h-auto md:h-3/4 lg:h-5/6 duration-300 hover:-translate-y-2'
+                            }
+                        />
                     </ComicCard>
                     
             ))}
